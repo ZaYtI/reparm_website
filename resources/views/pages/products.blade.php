88ced20c9fec90c461c8d55976076a67f6fa549a
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-09-07 16:45:01 +0200

    💄 Fix ui wrap for card element

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -3,13 +3,49 @@
         display: flex;
     }
 
-    .select-cat {
-        min-width: 300px;
-        border-right: 2px solid grey;
+    .product-card-ctr {
+        padding: 1rem;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        gap: 1rem;
+        transition: all .5s ease-in-out;
     }
 
     .product-card {
-        min-width: 30%;
+        max-width: 350px;
+        width: 100%;
+        border-radius: 0.5rem;
+        overflow: hidden;
+        background: rgba(15, 10, 10, 0.31);
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(5px);
+        -webkit-backdrop-filter: blur(5px);
+        border: 1px solid rgba(15, 10, 10, 0.3);
+    }
+
+    .label {
+        width: 10px;
+        height: 10px;
+        border-radius: 50%;
+    }
+
+    .red {
+        background-color: red;
+    }
+
+    .green {
+        background-color: green;
+    }
+
+    .has-stock {
+        display: flex;
+        align-items: center;
+    }
+
+    .label-text {
+        margin-left: 0.5rem;
+        color: white;
     }
 </style>
 
@@ -17,91 +53,25 @@
     <x-banner />
     <x-top-section-title title="Nos Fusils" :decoration="true" />
     <div class="container mx-auto product-ctr">
-        <div class="select-cat">
-            <ul class="text:white">
-                <li class="p-3">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox"
-                            class="rounded white:bg-gray-900 border-gray-300 white:border-gray-700 text-blue-600 shadow-sm white:focus:ring-offset-gray-800"
-                            name="remember">
-                        <span
-                            class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Se souvenir de moi') }}</span>
-                    </label>
-                </li>
-                <li class="p-3">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox"
-                            class="rounded white:bg-gray-900 border-gray-300 white:border-gray-700 text-blue-600 shadow-sm white:focus:ring-offset-gray-800"
-                            name="remember">
-                        <span
-                            class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Se souvenir de moi') }}</span>
-                    </label>
-                </li>
-                <li class="p-3">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox"
-                            class="rounded white:bg-gray-900 border-gray-300 white:border-gray-700 text-blue-600 shadow-sm white:focus:ring-offset-gray-800"
-                            name="remember">
-                        <span
-                            class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Se souvenir de moi') }}</span>
-                    </label>
-                </li>
-                <li class="p-3">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox"
-                            class="rounded white:bg-gray-900 border-gray-300 white:border-gray-700 text-blue-600 shadow-sm white:focus:ring-offset-gray-800"
-                            name="remember">
-                        <span
-                            class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Se souvenir de moi') }}</span>
-                    </label>
-                </li>
-            </ul>
-        </div>
-        <div class="grid grid-rows-* grid-col-4 gap-4 px-2">
+        <div class="product-card-ctr">
             @foreach ($products as $product)
-                <div
-                    class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class=" product-card">
                     <a href="#">
                         <img src="{{ asset('images/banniere_accueil.jpg') }}" alt="">
                     </a>
-                    <div class="px-5 pb-5">
+                    <div class="px-5 pb-5 py-2">
                         <a href="#">
                             <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                                 {{ $product->name }}</h5>
                         </a>
-                        <div class="flex items-center mt-2.5 mb-5">
-                            <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path
-                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path
-                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path
-                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path
-                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
-                                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                                    <path
-                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                </svg>
+                        <div class="has-stock">
+                            <div class="label green"></div>
+                            <div class="label-text">
+                                En stock
                             </div>
-                            <span
-                                class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                         </div>
                         <div class="flex items-center justify-between">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ $product->price }}</span>
+                            <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ $product->price }}€</span>
                             <a href="#"
                                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter
                                 au panier</a>
